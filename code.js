aa75c07d88e61def0247b910cec85acbea250806
Leonard Karoly <leonard.a.karoly@gmail.com>
2022-11-22 14:57:03 -0600

    add the frist test and solution for intro-to-testing

--- code.js
+++ code.js
@@ -1,4 +1,5 @@
 // helloWorld function 
-function helloWorld() {
-    return "Hello, World!";
-}
+const helloWorld = function() {
+    return "Hello, World!"; //changing to just 'Hello' causes the last test to fail
+} // What do you notice about the test results? Everything goes Red and the site doesn't like the function 'hello'
+const sayHello = () => `Hello`;
--- tests.js
+++ tests.js
@@ -1,15 +1,1 @@
-// Unit tests for the helloWorld function
-describe('helloWorld', function() {
-    it('should be a defined function', function() {
-      expect(typeof helloWorld).toBe('function');
-    });
-    it('should return a string when called', function() {
-      expect(typeof helloWorld()).toBe("string");
-    });
-    it('should return the string "Hello, World!" when executed', function() {
-      expect(helloWorld()).toBe("Hello, World!");
-    });
-    it("should never return 'undefined' when called", function() {
-      expect(helloWorld()).not.toBe(undefined);
-    });
-});
+// Unit tests for the helloWorld functiondescribe('helloWorld', function() {    it('should be a defined function', function() {      expect(typeof helloWorld).toBe('function');    });    it('should return a string when called', function() {      expect(typeof helloWorld()).toBe("string");    });    it('should return the string "Hello, World!" when executed', function() {      expect(helloWorld()).toBe("Hello, World!");    });    it("should never return 'undefined' when called", function() {      expect(helloWorld()).not.toBe(undefined);    });});describe('sayHello',function() {    it('Make It A Function!', function() {        expect(typeof sayHello).toBe('function');    });    it('Return A String!', function(){        expect(typeof sayHello()).toBe("string");    });    it('Does it say "Hello" ?', function(){        expect(sayHello()).toBe("Hello");    });    it('CANNOT BE "undefined" OR ELSE...', function(){        expect(sayHello()).not.toBe(undefined);    });});
