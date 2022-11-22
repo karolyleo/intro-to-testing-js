0562acb267d8501721448aafcc8ac8df47908fc5
Leonard Karoly <leonard.a.karoly@gmail.com>
2022-11-22 15:27:03 -0600

    add tests and ability to say 'hello' to any input.

--- code.js
+++ code.js
@@ -2,4 +2,4 @@
 const helloWorld = function() {
     return "Hello, World!"; //changing to just 'Hello' causes the last test to fail
 } // What do you notice about the test results? Everything goes Red and the site doesn't like the function 'hello'
-const sayHello = () => `Hello`;
+const sayHello = (name) =>  `Hello, ${name}!`; //cut out 'name==="Alex" ? 'Hello, Alex!' : name==='Pat' ? `Hello, Pat!` : `Hello, ${name}!`;
